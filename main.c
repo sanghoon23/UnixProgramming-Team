504e16cff2795af9ce827c8a4938754b57f73f97
ljyok117 <ljyok117@naver.com>
2016-12-03 18:02:43 +0900

    final

--- main.c
+++ main.c
@@ -25,6 +25,8 @@ typedef struct splitChar  //기본 구조체
 char * accessAuth(int mode); // 권항
 splitChar * makeStruct(); // 구조체 선언
 void initStruct(splitChar *p); // 해당 구조체 0으로 초기화
+splitChar * splitPermString(char *perm);
+void routinePerm(splitChar *p, char name[], int i);
 
 int main(int argc, char *argv[])
 {
@@ -47,7 +49,65 @@ int main(int argc, char *argv[])
 	p = makeStruct(p);									// 구조체 생성 / 포인터 변수 연결
 	p->structPresent = 1;									// 구조체 상태를 1으로
 
-	chmod(argv[1], buf.st_mode);
+	while(p->structPresent==1)								// until structPresent that finds ',' in string argv[1] doesnt become 1
+	{
+	    	if((argv[1][0]>='0'&&argv[1][0]<='7')&&(argv[1][1]>='0'&&argv[1][1]<='7')
+		    &&(argv[1][2]>='0'&&argv[1][2]<='7')&&(argv[1][3]>='0'&&argv[1][3]<='7')
+		    &&(strlen(argv[1])==4))							// if the authority is the number mode and have 4 length ex>>4644
+		{
+		    changePermNumMode(argv[2], argv[1]);
+
+		    break;
+		}
+
+	    	if((argv[1][0]>='0'&&argv[1][0]<='7')&&(argv[1][1]>='0'&&argv[1][1]<='7')
+		    &&(argv[1][2]>='0'&&argv[1][2]<='7')&&(strlen(argv[1])==3))			// if the authority is the number mode and have 3 length ex>>644
+		{
+		    changePermNumMode(argv[2], argv[1]);
+
+		    break;
+		}
+		    
+		p = splitPermString(argv[1]+pointArray);					// cut the argv[1] as of char ',' and check the authority is char mode
+
+		for(k=0; k<4; k++)
+		{
+			if(p->user[0]=='\0')							// if there are no char like 'u', 'g', 'o', it thinks 'a'
+			{
+				routinePerm(p, argv[2], 0);
+				routinePerm(p, argv[2], 1);
+				routinePerm(p, argv[2], 2);
+
+				if(p->command=='=')						// buf if the command is '=', we have to minus authority as umask
+				{
+				    mask = readUmaskInFile(storeUmaskInFile());			// store umask value in pointer value
+
+				    mask_int[0]=(int)(*(mask+0)-48);				// change asc to int, store user umask in mask_int[0]
+				    mask_int[1]=(int)(*(mask+1)-48);				// change asc to int, store user umask in mask_int[1]
+				    mask_int[2]=(int)(*(mask+2)-48);				// change asc to int, store user umask in mask_int[2]
+
+				    minusAuth_usingUmask(argv[2], mask_int);			// minus authority as much as umask
+				}
+
+				break;
+			}
+			else if(p->user[k]=='\0')						// we dont have to still go 'for loop' despite the string(p->user[k]) is ended
+				break;
+		
+			if(p->user[k]=='u')							// p->user[k] is 'u'
+				routinePerm(p, argv[2], 0);
+			else if(p->user[k]=='g')
+				routinePerm(p, argv[2], 1);					// p->user[k] is 'g'
+			else if(p->user[k]=='o')
+				routinePerm(p, argv[2], 2);					// p->user[k] is 'o'
+			else if(p->user[k]=='a')
+			{
+				routinePerm(p, argv[2], 0);					// p->user[k] is 'u'
+				routinePerm(p, argv[2], 1);					// p->user[k] is 'g'
+				routinePerm(p, argv[2], 2);					// p->user[k] is 'o'
+			}
+		}
+	}
 
 	lstat(argv[2], &buf); // 해당 파일의 정보를 받아온다. buf에 저장
 	printf("After change authority : %s\n", accessAuth(buf.st_mode)); // 바뀐후 권한
@@ -120,6 +180,87 @@ void initStruct(splitChar *p) // 구조체 변수들 초기화
 	}
 	p->command = '\0';
 	p->structPresent = 1;
+}
+
+splitChar * splitPermString(char *perm) // split parameter member as of char ',' then return the string after ','
+{
+	static splitChar p;
+	int i = 0;
+	
+	initStruct(&p);
+
+	// save string that is before command('+', '-', '=') in p.user
+	while(((*perm)!='+')&&((*perm)!='-')&&((*perm)!='='))
+	{
+		if(((*perm)!='u')&&((*perm)!='g')&&((*perm)!='o')&&((*perm)!='a'))
+		{
+			printf("wrong input\n");
+
+			exit(1);
+		}
+		p.user[i++] = *perm++;
+		pointArray++;
+
+		if(i==4)
+		{
+			printf("wrong input\n");
+			
+			exit(1);
+		}
+	}
+	p.user[i] = '\0';
+	
+	// save char that is '+', '-' or '=' in p.command
+	if(((*perm)!='+')&&((*perm)!='-')&&((*perm)!='='))
+	{
+		printf("wrong input\n");
+
+		exit(1);
+	}
+	p.command = *perm++;
+	pointArray++;
+
+	if((*perm)=='\0')
+	{
+		printf("wrong input\n");
+
+		exit(1);
+	}
+
+	i = 0;
+
+	// save string that is after command('+', '-', '=') in p.permission
+	while((*perm)!='\0')
+	{
+		if(((*perm)!='r')&&((*perm)!='w')&&((*perm)!='x'))
+		{
+			printf("wrong input\n");
+
+			exit(1);
+		}
+		p.permission[i++] = *perm++;
+		pointArray++;
+
+		if((*perm)==',')
+		{
+			p.structPresent = 1;
+			p.permission[i] = '\0';
+
+			pointArray++;
+
+			return &p;
+		}
+		else if(i==4)
+		{
+			printf("wrong input\n");
+
+			exit(1);
+		}
+	}
+	p.permission[i] = '\0';
+	p.structPresent = 0;
+
+	return &p;
 }
 
 void routinePerm(splitChar *p, char name[], int i) 		// 퍼미션 주는 것
